658b040f63f57259440250adcc40c1028b5501a5
Alvechen <alvechen@alvechendemacbook-pro.local>
2015-11-23 17:41:04 +0800

    Initial Commit

---
+++ 08-managedObjectContext查询/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  08-managedObjectContext查询
+//
+//  Created by Alvechen on 15/11/23.
+//  Copyright © 2015年 Alvechen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
